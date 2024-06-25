e7038bd027f80b2fde29f9c8c9c825822d69512a
Yaghoub Adelzadeh <adelzadeh74@gmail.com>
2024-06-25 12:34:26 +0330

    Update Example.sol

--- BlockTimeStampManipulation/Example.sol
+++ BlockTimeStampManipulation/Example.sol
@@ -39,3 +39,11 @@ contract Roulette {
         }
     }
 }
+/*
+Vulnerability:
+block.timestamp can be manipulated by miners with the following constraints
+    * it cannot be stamped with an earlier time than its parent
+    * it cannot be too far in the future
+Preventative Techniques:
+Don't use block.timestamp for a source of entropy and random number
+*/
